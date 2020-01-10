a8e01ea1dec6451740c740a36d7c18ffddee6e71
Thomas Harbin <thomasharbin@Thomass-MBP-2.attlocal.net>
2020-01-10 09:40:36 -0600

    removed environement variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,9 +39,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: process.env.API_URL,
+        baseUrl: "wpreact.local",
         // The protocol. This can be http or https.
-        protocol: process.env.API_PROTOCOL,
+        protocol: "https",
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
